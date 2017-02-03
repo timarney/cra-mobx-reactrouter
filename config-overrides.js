3487a89ba82848e21bdc61dcdfdadada13416bb6
Tim Arney <tim@line37.com>
2017-02-03 15:08:34 -0500

    add preact rewire

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,8 @@
 const rewireMobX = require("react-app-rewire-mobx");
+const rewirePreact = require("react-app-rewire-preact");
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
-  config = rewireMobX(config, env);
+  config = rewirePreact(rewireMobX(config, env));
   return config;
 };
